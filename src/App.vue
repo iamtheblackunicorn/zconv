5c46bdb335599a81326ce9ac247be27953383855
iamtheblackunicorn <officialrealalexanderabraham@gmail.com>
2022-06-29 17:54:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+ <NumConverter/>
+</template>
+
+<script>
+import NumConverter from './components/NumConverter.vue';
+export default {
+  name: 'App',
+  components: {
+    NumConverter
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/NumConverter.vue
@@ -0,0 +1,51 @@
+<template>
+ <input v-model="start" placeholder="Expression"/>
+ <button @click="convertNum">Compute</button>
+ <button @click="changeMode">Switch Mode</button>
+ <p>Mode: {{ mode }}</p>
+ <p>Result: {{ result }}</p>
+</template>
+
+<script>
+import zeppo from 'zeppo';
+export default {
+  
+  name: 'NumConverter',
+  data() {
+    
+    return {
+       start: '',
+       result: '',
+       mode: 'bin'
+    };
+  },
+  methods: {
+    changeMode(){
+        if (this.mode === 'bin'){
+            this.mode = 'dec';
+        }
+        else {
+            this.mode = 'bin';
+        }
+    },
+    convertNum(){
+        if (this.mode === 'bin'){
+            if (zeppo.isBin(this.start) === true){
+                this.result = zeppo.binToDec(this.start);
+            }
+            else {
+                this.result = 'You fucking moron rofl.'
+            }
+        }
+        else {
+            if (zeppo.isInt(this.start) === true){
+                this.result = zeppo.decToBin(this.start);
+            }
+            else {
+                this.result = 'You fucking moron rofl.'
+            }
+        }
+    }
+  }  
+}
+</script>
